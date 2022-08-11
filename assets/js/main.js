2819cbacb2971066711974f6c3213b6c54abd5b5
fonovka <brkictamara7@gmail.com>
2022-08-11 15:37:59 +0200

    formatiranje koda

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,27 +11,14 @@ function dohvatiPodatke(nazivIzvora, callBack) {
     xhr.send();
 }
 
+dohvatiPodatke("pisci.json", ispisiFilterPisaca);
+dohvatiPodatke("zanrovi.json", ispisiZanrove);
+dohvatiPodatke("knjige.json", ispisiKnjige);
+
 let pisci;
 let zanrovi;
 
-let stanje = document.querySelectorAll(".stanje");
-stanje.forEach(elem => {
-    elem.addEventListener("change", filterChange);
-})
-
-document.getElementById("pretraga").addEventListener("keyup", filterChange);
-
-document.getElementById("rang").addEventListener("change", filterChange);
-
-document.getElementById("sort").addEventListener("change", filterChange);
-
-// function ispisiFilterPisaca(data) {
-//     console.log(data);
-// }
-
-//arrow function expression
-const ispisiFilterPisaca = (data) => {
-    // console.log(data);
+function ispisiFilterPisaca(data) {
     pisci = data;
     let ispis = '';
     data.forEach(elem => {
@@ -46,10 +33,6 @@ const ispisiFilterPisaca = (data) => {
         chekPisci[i].addEventListener("change", filterChange);
     }
 }
-
-dohvatiPodatke("pisci.json", ispisiFilterPisaca);
-dohvatiPodatke("zanrovi.json", ispisiZanrove);
-dohvatiPodatke("knjige.json", ispisiKnjige);
 
 function ispisiZanrove(data) {
     zanrovi = data;
@@ -67,8 +50,145 @@ function ispisiZanrove(data) {
     }
 }
 
+function ispisiKnjige(data) {
+    data = filterCena(data);
+    data = filterPicsi(data);
+    data = filterZanr(data);
+    data = filterStanje(data);
+    data = filterNaslov(data);
+    data = sortKnjiga(data);
+    countPoCeni(data);
+    countZanr(data);
+    countPisac(data);
+    countStanja(data);
+    countPretragaPoNaslovu(data);
+    let ispis = "";
+    data.forEach(book => {
+        ispis += `<div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+        <img src="assets/img/${book.slika.src}" class="card-img-top" alt="${book.naslov}">
+        <div class="card-body">
+          <h3 class="card-title">${book.naslov}</h3>
+          <h4>${dohvatiPisca(book.pisacID)}</h4>
+          <h5>$${book.price.novaCena}</h5>
+          <h6><s>$${book.price.staraCena}</s></h6>
+          <p class="text-primary">${book.naStanju ? "Knjiga je dostupna" : "Knjiga nije dostupna"}</p>
+          <p class="card-text">${dohvatiZanrove(book.zanrovi)}</p>
+          <button class="btn btn-primary korpaText" data-id="${book.id}" data-naslov="${book.naslov}" data-cena="${book.price.novaCena}">Ubaci u korpu</button>
+        </div>
+        </div>
+        </div>`
+    });
+    if (data.length == 0) {
+        ispis = "Nema knjiga";
+    }
+    document.getElementById("knjige").innerHTML = ispis;
+
+    var dugmici = document.querySelectorAll(".korpaText");
+    dugmici.forEach(elem => {
+        elem.addEventListener("click", dodajUKorpu);
+    })
+}
+
+function dohvatiPisca(id) {
+    let nizIme = pisci.filter(elem => elem.id == id);
+    let ime = nizIme[0].ime;
+    let prezime = nizIme[0].prezime;
+    return ime + " " + prezime;
+}
+
+function dohvatiZanrove(nizZ) {
+    let ispis = "";
+    let zanroviN = zanrovi.filter(elem => nizZ.includes(elem.id));
+
+    for (let i = 0; i < zanroviN.length; i++) {
+        ispis += zanroviN[i].naziv;
+        if (i != zanroviN.length - 1) {
+            ispis += ", ";
+        }
+    }
+
+    return ispis;
+}
+
+let stanje = document.querySelectorAll(".stanje");
+stanje.forEach(elem => {
+    elem.addEventListener("change", filterChange);
+})
+
+document.getElementById("pretraga").addEventListener("keyup", filterChange);
+
+document.getElementById("rang").addEventListener("change", filterChange);
+
+document.getElementById("sort").addEventListener("change", filterChange);
+
 function filterChange() {
     dohvatiPodatke("knjige.json", ispisiKnjige);
+}
+
+function filterPicsi(data) {
+    let nizChekiranihVrednosti = [];
+    let nizCh = document.querySelectorAll(".pisac");
+    nizCh.forEach(
+        elem => {
+            if (elem.checked) {
+                nizChekiranihVrednosti.push(parseInt(elem.value));
+            }
+        }
+    )
+    if (nizChekiranihVrednosti.length != 0) {
+        return data.filter(elem => nizChekiranihVrednosti.includes(elem.pisacID));
+    }
+    return data;
+}
+
+function filterZanr(data) {
+    let nizChekiranihVrednosti = [];
+    let nizCh = document.querySelectorAll(".zanr");
+    nizCh.forEach(
+        elem => {
+            if (elem.checked) {
+                nizChekiranihVrednosti.push(parseInt(elem.value));
+            }
+        }
+    )
+    if (nizChekiranihVrednosti.length != 0) {
+        return data.filter(elem => elem.zanrovi.some(zanrID => nizChekiranihVrednosti.includes(zanrID)));
+    }
+    return data;
+}
+
+function filterStanje(data) {
+    let chekirani = document.querySelector(".stanje:checked");
+
+    if (chekirani.value == "dostupno") {
+        return data.filter(elem => elem.naStanju)
+    }
+    if (chekirani.value == "nedostupno") {
+        return data.filter(elem => !elem.naStanju)
+    }
+    return data;
+}
+
+function filterNaslov(data) {
+    let val = document.getElementById("pretraga").value;
+    if (val) {
+        return data.filter(elem => elem.naslov.toLowerCase().indexOf(val.trim().toLowerCase()) != -1);
+    }
+    return data;
+}
+
+function filterCena(data) {
+    let cenat = parseInt(document.getElementById("rang").value);
+    return data.filter(elem => parseInt(elem.price.novaCena) <= cenat);
+}
+
+function sortKnjiga(data) {
+    let metod = document.querySelector("#sort").value;
+    if (metod == "asc") {
+        return data.sort((a, b) => parseInt(a.price.novaCena) > parseInt(b.price.novaCena) ? 1 : -1);
+    }
+    return data.sort((a, b) => parseInt(a.price.novaCena) < parseInt(b.price.novaCena) ? 1 : -1);
 }
 
 function countZanr(data) {
@@ -158,159 +278,12 @@ function countPoCeni(data) {
     document.getElementById("rez").textContent = `${cenat} (${data.length})`;
 }
 
-function ispisiKnjige(data) {
-    data = filterCena(data);
-    data = filterPicsi(data);
-    data = filterZanr(data);
-    data = filterStanje(data);
-    data = filterNaslov(data);
-    data = sortKnjiga(data);
-    countPoCeni(data);
-    countZanr(data);
-    countPisac(data);
-    countStanja(data);
-    countPretragaPoNaslovu(data);
-    let ispis = "";
-    data.forEach(book => {
-        ispis += `<div class="col-lg-4 col-md-6 mb-4">
-        <div class="card h-100">
-        <img src="assets/img/${book.slika.src}" class="card-img-top" alt="${book.naslov}">
-        <div class="card-body">
-          <h3 class="card-title">${book.naslov}</h3>
-          <h4>${dohvatiPisca(book.pisacID)}</h4>
-          <h5>$${book.price.novaCena}</h5>
-          <h6><s>$${book.price.staraCena}</s></h6>
-          <p class="text-primary">${book.naStanju ? "Knjiga je dostupna" : "Knjiga nije dostupna"}</p>
-          <p class="card-text">${dohvatiZanrove(book.zanrovi)}</p>
-          <button class="btn btn-primary korpaText" data-id="${book.id}" data-naslov="${book.naslov}" data-cena="${book.price.novaCena}">Ubaci u korpu</button>
-        </div>
-        </div>
-        </div>`
-    });
-    if (data.length == 0) {
-        ispis = "Nema knjiga";
-    }
-    document.getElementById("knjige").innerHTML = ispis;
-
-    var dugmici = document.querySelectorAll(".korpaText");
-    dugmici.forEach(elem => {
-        elem.addEventListener("click", dodajUKorpu);
-    })
-}
-
-// let stanje = document.querySelectorAll(".stanje");
-
-function dohvatiPisca(id) {
-    // console.log(pisci);
-    let nizIme = pisci.filter(elem => elem.id == id);
-    // console.log(nizIme);
-    let ime = nizIme[0].ime;
-    let prezime = nizIme[0].prezime;
-    // console.log(ime + " " + prezime);
-    return ime + " " + prezime;
-}
-
-function dohvatiZanrove(nizZ) {
-    let ispis = "";
-    // console.log(nizZ);
-    let zanroviN = zanrovi.filter(elem => nizZ.includes(elem.id));
-    // console.log(zanroviN);
-
-    // for (let i = 0; i < nizZ.length; i++) {
-    //     for (let j = 0; j < zanrovi.length; j++) {
-    //         if (nizZ[i] == zanrovi[j].id) {
-    //             ispis += zanrovi[j].naziv;
-    //             if (i != nizZ.length - 1) {
-    //                 ispis += ", ";
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
-
-    for (let i = 0; i < zanroviN.length; i++) {
-        ispis += zanroviN[i].naziv;
-        if (i != zanroviN.length - 1) {
-            ispis += ", ";
-        }
-    }
-
-    // console.log(ispis);
-
-    return ispis;
-}
-
-function filterPicsi(data) {
-    let nizChekiranihVrednosti = [];
-    let nizCh = document.querySelectorAll(".pisac");
-    nizCh.forEach(
-        elem => {
-            if (elem.checked) {
-                nizChekiranihVrednosti.push(parseInt(elem.value));
-            }
-        }
-    )
-    if (nizChekiranihVrednosti.length != 0) {
-        return data.filter(elem => nizChekiranihVrednosti.includes(elem.pisacID));
-    }
-    return data;
-}
-
-function filterZanr(data) {
-    let nizChekiranihVrednosti = [];
-    let nizCh = document.querySelectorAll(".zanr");
-    nizCh.forEach(
-        elem => {
-            if (elem.checked) {
-                nizChekiranihVrednosti.push(parseInt(elem.value));
-            }
-        }
-    )
-    if (nizChekiranihVrednosti.length != 0) {
-        return data.filter(elem => elem.zanrovi.some(zanrID => nizChekiranihVrednosti.includes(zanrID)));//[1, 2]
-    }
-    return data;
-}
-
-function filterStanje(data) {
-    let chekirani = document.querySelector(".stanje:checked");
-    // console.log(chekirani);
-    if (chekirani.value == "dostupno") {
-        return data.filter(elem => elem.naStanju)
-    }
-    if (chekirani.value == "nedostupno") {
-        return data.filter(elem => !elem.naStanju)
-    }
-    return data;
-}
-
-function filterNaslov(data) {
-    let val = document.getElementById("pretraga").value;
-    if (val) {
-        return data.filter(elem => elem.naslov.toLowerCase().indexOf(val.trim().toLowerCase()) != -1);
-    }
-    return data;
-}
-
-function filterCena(data) {
-    let cenat = parseInt(document.getElementById("rang").value);
-    // document.getElementById("rez").textContent = `${cenat} (${data.length})`;
-    return data.filter(elem => parseInt(elem.price.novaCena) <= cenat);
-}
-
-function sortKnjiga(data) {
-    let metod = document.querySelector("#sort").value;
-    if (metod == "asc") {
-        return data.sort((a, b) => parseInt(a.price.novaCena) > parseInt(b.price.novaCena) ? 1 : -1);
-    }
-    return data.sort((a, b) => parseInt(a.price.novaCena) < parseInt(b.price.novaCena) ? 1 : -1);
-}
+checkCart();
 
 function dodajUKorpu() {
-    // let id = this.dataset.id;
     let naslov = this.dataset.naslov;
     let cena = this.dataset.cena;
-    // console.log(naslov);
+
     let cart = [];
 
     const cookieCart = document.cookie.split("; ").find(row => row.startsWith("cart="));
@@ -366,8 +339,6 @@ function checkCart() {
     })
 }
 
-checkCart();
-
 function removeFromChart() {
     let naslov = this.dataset.book;
 
@@ -389,11 +360,8 @@ function removeFromChart() {
 }
 
 function azurirajQuantity() {
-    console.log("na dobrom si putu");
-
-    // let id = this.dataset.id;
     let naslov = this.dataset.book;
-    // console.log(naslov);
+
     let cart = [];
 
     const cookieCart = document.cookie.split("; ").find(row => row.startsWith("cart="));
